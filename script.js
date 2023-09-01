6854f5c520bff5fc751ed4d31c7fd9da477bfb3c
Henecito <116605593+Henecito@users.noreply.github.com>
2023-09-01 13:38:21 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const users = {
+        henecito: "henecito",
+    };
+
+    const loginForm = document.getElementById("login-form");
+    const message = document.getElementById("message");
+
+    loginForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+        const username = document.getElementById("username").value;
+        const password = document.getElementById("password").value;
+
+        if (users[username] === password) {
+            message.textContent = "Inicio de sesión exitoso!";
+            message.style.color = "white";
+            message.classList.remove("hidden");
+            window.location.href = "principal.html";
+        } else {
+            message.textContent = "Credenciales incorrectas. Inténtalo de nuevo.";
+            message.style.color = "red";
+            message.classList.remove("hidden");
+        }
+    });
+
+    const forgotPasswordLink = document.getElementById("forgot-password");
+    forgotPasswordLink.addEventListener("click", function (e) {
+        e.preventDefault();
+        alert("¡Has solicitado restablecer tu contraseña!");
+    });
+});
