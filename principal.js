43298eaedd6e92d459ee5a40f7d06ec98f172e02
Henecito <116605593+Henecito@users.noreply.github.com>
2023-09-02 04:08:32 -0400

    Add files via upload

--- principal.js
+++ principal.js
@@ -2,16 +2,33 @@ const openTableButtons = document.querySelectorAll('.open-table-button');
 const formContainer = document.getElementById('form-container');
 const tables = document.querySelectorAll('.table')
 const formP = document.querySelector('.form-container-p')
-
+const areaButtons = document.querySelectorAll('.area-button');
 const templateContainer = document.querySelector('.template__container')
 const template = document.querySelector('#templateCreatedOrder').content
 const fragment = document.createDocumentFragment();
 
-
 const info = {}
 
-openTableButtons.forEach(elem => {
+areaButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const area = button.getAttribute('data-area');
+        showTablesInArea(area);
+    });
+});
 
+function showTablesInArea(area) {
+    tables.forEach(table => {
+        if (table.getAttribute('data-area') === area) {
+            table.style.display = 'block';
+        } else {
+            table.style.display = 'none';
+        }
+    });
+}
+
+showTablesInArea('salon');
+
+openTableButtons.forEach(elem => {
     elem.addEventListener('click', (e) => {
         if (!e.target.parentNode.classList.contains('active-table')) {
             openTableButtons.forEach(e => {
@@ -33,14 +50,12 @@ openTableButtons.forEach(elem => {
             const formContent = formContainer.querySelector('.open-table-form');
             formContent.style.display = 'flex'
             const spanId = formContent.querySelector('#id-table')
-    
+
             spanId.textContent = e.target.parentNode.getAttribute('data-table-id')
             const form = formContent.querySelector('form');
 
             form.addEventListener('submit', (e) => {
                 e.preventDefault()
-
-                console.log(e.target.querySelector('#garzon').value);
 
                 if (!e.target.querySelector('#garzon').value == '') {
                     selectTable(e)
@@ -85,12 +100,10 @@ const createOrder = (table) => {
     const id = table.getAttribute('data-table-id')
 
     templateContainer.innerHTML = ''
-    
 
     table.classList.add('order__created');
 
     formContainer.querySelector('.open-table-form').style.display = 'none';
-
 
     const clone = template.cloneNode(true)
 
@@ -106,8 +119,25 @@ const createOrder = (table) => {
         formContainer.querySelector('.open-table-form').querySelector('form').reset()
     })
 
-
     fragment.appendChild(clone)
 
     templateContainer.appendChild(fragment)
 }
+
+const dateElement = document.getElementById("date");
+const timeElement = document.getElementById("time");
+
+function updateDateTime() {
+    const now = new Date();
+    const optionsDate = { weekday: "long", year: "numeric", month: "long", day: "numeric" };
+    const optionsTime = { hour: "2-digit", minute: "2-digit" };
+    const formattedDate = now.toLocaleDateString("es-ES", optionsDate);
+    const formattedTime = now.toLocaleTimeString("es-ES", optionsTime);
+
+    dateElement.textContent = formattedDate;
+    timeElement.textContent = formattedTime;
+}
+
+setInterval(updateDateTime, 1000);
+
+updateDateTime();
